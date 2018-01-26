94c9eddd16839971e8e5a697bd1e6fd201fe53e9
Xiaobo Deng <xiaobo.deng@comfirmit.com>
2018-01-26 14:55:07 +0100

    initial commit

---
+++ typescript/app.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+import "./tsStyles.scss";
+
+export const TSApp = () => {
+  return (
+    <h1 className="ts-react">Hello, React with Typescript!</h1>
+  );
+};
---
+++ typescript/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {TSApp} from "./app";
+
+const rootDiv = document.getElementById("tsapp");
+ReactDOM.render(<TSApp/>, rootDiv);
